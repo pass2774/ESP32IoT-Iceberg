9433f8b0065b21ef39e1ad9cebbe43c7228b40c7
Hyeokjun Yoon <yhjdbs@gmail.com>
2023-05-19 15:27:39 +0900

    first

--- BLE_MQTT/BLE_MQTT.ino
+++ BLE_MQTT/BLE_MQTT.ino
@@ -19,6 +19,7 @@
    In this example rxValue is the data received (only accessible inside that function).
    And txValue is the data to be sent, in this example just a byte incremented every second. 
 */
+//test1
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
